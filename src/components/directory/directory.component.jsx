1998e08c00888834cefa7136f074c04474e56f42
Davyd Bredykhin <correodemimobil@gmail.com>
2022-09-12 12:38:07 +0200

    studied new concept of route. The nav bar was added successfully

--- src/components/directory/directory.component.jsx
+++ src/components/directory/directory.component.jsx
@@ -7,7 +7,7 @@ const Directory = ({categories}) => {
     <div className="directory-container">
       {categories.map((category) => (
         <CategoryItem key={category.id} category={category}/>
-      ))};
+      ))}
     </div>
   );
 };
