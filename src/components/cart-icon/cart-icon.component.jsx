c1fbb4c9b432d7c3687638a1275261a3c668dd0e
Davyd Bredykhin <correodemimobil@gmail.com>
2022-11-28 22:40:23 +0100

    click outside fix

--- src/components/cart-icon/cart-icon.component.jsx
+++ src/components/cart-icon/cart-icon.component.jsx
@@ -7,14 +7,13 @@ const CartIcon = ({addClass}) => {
   const { isCartOpen, setIsCartOpen, cartCount } = useContext(CartContext);
   const toggleIsCartOpen = () => setIsCartOpen(!isCartOpen);
   const cartRef = useRef();
-  const clickOutsideCart = (e) => {
-    if(!e.path.includes(cartRef.current)) {
-      setIsCartOpen(false);
-    }
-  }
 
   useEffect(() => {
-    document.querySelector("body").addEventListener("click", e => clickOutsideCart);
+    document.querySelector("body").addEventListener("click", e => {
+      if(!e.path.includes(cartRef.current)) {
+        setIsCartOpen(false);
+      }
+    });
   }, []);
 
   return (
